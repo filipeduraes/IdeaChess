d5c993640c7bc3111516cbf1d60848976b7f9edf
Filipe Durães <77279496+filipeduraes@users.noreply.github.com>
2026-02-18 14:29:40 -0300

    Fix SDL Main issues and add minimum example

--- src/private/main.cpp
+++ src/private/main.cpp
@@ -1,22 +1,55 @@
 #include <SDL.h>
+#include <cstdlib>
+
+namespace
+{
+    constexpr int WindowWidth = 800;
+    constexpr int WindowHeight = 600;
+    constexpr const char* WindowTitle = "SDL Window";
+}
 
 int main()
 {
-    SDL_Init(SDL_INIT_VIDEO);
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        return EXIT_FAILURE;
+    }
 
-    SDL_Window* window = SDL_CreateWindow(
-        "SDL",
+    SDL_Window* window = SDL_CreateWindow
+    (
+        WindowTitle,
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        640,
-        480,
-        0
+        WindowWidth,
+        WindowHeight,
+        SDL_WINDOW_SHOWN
     );
 
-    SDL_Delay(3000);
+    if (window == nullptr)
+    {
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    bool isRunning = true;
+
+    while (isRunning)
+    {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event) != 0)
+        {
+            if (event.type == SDL_QUIT)
+            {
+                isRunning = false;
+            }
+        }
+
+        SDL_Delay(1);
+    }
 
     SDL_DestroyWindow(window);
     SDL_Quit();
 
-    return 0;
+    return EXIT_SUCCESS;
 }
